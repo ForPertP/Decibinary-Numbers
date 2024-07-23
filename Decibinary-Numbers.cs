9f49053bad127970232d8f0d950f7a3a351f9f23
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-24 08:42:48 +0900

    Update Decibinary-Numbers.cs

--- Decibinary-Numbers.cs
+++ Decibinary-Numbers.cs
@@ -1,1 +1,32 @@
-cs
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'decibinaryNumbers' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER x as parameter.
+     */
+
+    public static long decibinaryNumbers(long x)
+    {
+        long result = 0L;
+        return result;
+    }
+
+    
+}
