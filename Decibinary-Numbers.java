5aa9cc45e0b063d76e3561886bd98ef68703cc4a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-08-02 07:56:53 +0900

    Update Decibinary-Numbers.java

--- Decibinary-Numbers.java
+++ Decibinary-Numbers.java
@@ -1,1 +1,28 @@
-Java
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'decibinaryNumbers' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER x as parameter.
+     */
+
+    private static final int MAX_DIGITS = 10;
+    private static final int MAX_POWERS = 20;
+    private static final int MAX_DECIMAL_VALUE = 286000;
+  
+
+}
+
