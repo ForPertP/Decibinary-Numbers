514677f7eda41f9cca3ae64f7d5b54310c3387ab
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-08-14 02:32:45 +0900

    Update Decibinary-Numbers.cs

--- Decibinary-Numbers.cs
+++ Decibinary-Numbers.cs
@@ -91,13 +91,13 @@ class Result
         {
             int binaryPlaceValue = 1 << j;
 
-            for (int digit = 0; digit < MAX_DIGITS; ++digit)
+            for (int k = 0; k < MAX_DIGITS; ++k)
             {
-                int remainingValue = decimalValue - digit * binaryPlaceValue;
+                int remainingValue = decimalValue - k * binaryPlaceValue;
 
                 if (remainingValue >= 0 && offset < dpTable[remainingValue, j - 1])
                 {
-                    result = result * 10 + digit;
+                    result = result * 10 + k;
                     decimalValue = remainingValue;
                     break;
                 }
